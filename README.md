a82597e081f0c9e7b4e1b94815b5a78fda6342bf
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-11-04 10:26:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# great_places
+
+A great places diary project.
+
+Captures great locations, with images and geolocation/map documented and sub information.
+
+Utilizes device file storage, sqlite, camera and locations/maps.
---
+++ lib/providers/great_places.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:great_places/models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+}
