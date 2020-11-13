ab5ec217b8964932d319d82e4baa9d887ccfac95
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-11-13 12:26:44 +0100

    sqlite lite db name update

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -2,16 +2,24 @@ import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<sql.Database> database() async {
+    //open existing db or create new one
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(path.join(dbPath, 'places.db'),
+    return sql.openDatabase(path.join(dbPath, 'places.db'),
         onCreate: (db, version) {
       return db.execute(
           'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
-    }, version: 1); //open existing db or create new one
+    }, version: 1);
+  }
 
-    await sqlDb.insert(table, data,
-        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    db.insert(table, data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
     //last parameter ensures overwriting existing record with new one in event of a conflict
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
