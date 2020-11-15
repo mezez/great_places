d9659ebd1899e7fc291a01de251a196aaed512a5
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-11-15 21:38:34 +0100

    fetch and set places bug fix

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -30,7 +30,7 @@ class GreatPlaces with ChangeNotifier {
   Future<void> fetchAndSetPlaces() async {
     final dataList = await DBHelper.getData('user_places');
     //transform datalist
-    dataList
+    _items = dataList
         .map((item) => Place(
             id: item['id'],
             title: item['title'],
