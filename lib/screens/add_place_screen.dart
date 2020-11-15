82a10836b6b2039588f5e51456ee72e95c4f6ec4
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-11-15 22:27:06 +0100

    location input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'dart:io';
 
 import 'package:provider/provider.dart';
@@ -56,6 +57,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     height: 10,
                   ),
                   ImageInput(_selectImage),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  LocationInput(),
                 ],
               ),
             ),
